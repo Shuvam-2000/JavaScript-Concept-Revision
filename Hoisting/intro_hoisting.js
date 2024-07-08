e9945b094c189317dd64091dcba3bc8301a42565
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-07-08 16:42:41 +0530

    hoisiting in js completely revised

---
+++ Hoisting/intro_hoisting.js
@@ -0,0 +1,36 @@
+// hoisiting in js
+// example
+
+// function hello(){
+//     console.log("Hello World")
+// }
+
+// hoisting in fucntion 
+// hello();
+
+// function hello(){
+//     console.log("Hello World")
+// }
+
+// //variable
+// var x;  // declaration 
+
+// x = 2; // assignment
+
+// console.log(x);  //call 
+
+// // hoisting through variable
+// var x;  // declaration 
+
+// console.log(x);  //call
+
+// x = 2; // assignment
+
+
+// use strict  
+"use strict" 
+
+a = 4;
+
+console.log(a);
+
